f0ece2208f5c38d253eca487c2de9c0f82459e6b
10994015 <a0938599191@gmail.com>
2022-07-29 15:24:06 +0800

    0729

--- web/rank.php
+++ web/rank.php
@@ -15,71 +15,99 @@ session_start();
     <?php include_once('../shared/header.php'); ?>
     <div id="rank">
         <div class="rankbox">
-            <div class="item ranktitle">
-                <p class="p1">排名</p>
-                <p class="p2">累積獎金</p>
-                <p class="p3">會員帳號</p>
-                <p class="p4">名字</p>
+            <div class="top">
+                <div class="item">
+                    <img src="../images/no1.png" alt="">
+                    <div class="content">
+                        <h2 style="color:#F9DE83">1ST</h2>
+                        <p>zxc559995</p>
+                        <p>王曉明</p>
+                        <p>$1500000</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="../images/no2.png" alt="">
+                    <div class="content">
+                        <h2 style="color:#E8E9ED">2ST</h2>
+                        <p>zxc559995</p>
+                        <p>陳景宣</p>
+                        <p>$1500000</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="../images/no3.png" alt="">
+                    <div class="content">
+                        <h2 style="color:#B4967C">3ST</h2>
+                        <p>zxc559995</p>
+                        <p>黃昭和</p>
+                        <p>$1500000</p>
+                    </div>
+                </div>
+
             </div>
-            <div class="item no1">
-                <p class="p1">1</p>
-                <p class="p2">1500000</p>
-                <p class="p3">sxe7654</p>
-                <p class="p4">王O哲</p>
-            </div>
-            <div class="item">
-                <p class="p1">2</p>
-                <p class="p2">1500000</p>
-                <p class="p3">sxe7654</p>
-                <p class="p4">王O哲</p>
-            </div>
-            <div class="item">
-                <p class="p1">3</p>
-                <p class="p2">1500000</p>
-                <p class="p3">sxe7654</p>
-                <p class="p4">王O哲</p>
-            </div>
-            <div class="item">
-                <p class="p1">4</p>
-                <p class="p2">1500000</p>
-                <p class="p3">sxe7654</p>
-                <p class="p4">王O哲</p>
-            </div>
-            <div class="item">
-                <p class="p1">5</p>
-                <p class="p2">1500000</p>
-                <p class="p3">sxe7654</p>
-                <p class="p4">王O哲</p>
-            </div>
-            <div class="item">
-                <p class="p1">6</p>
-                <p class="p2">1500000</p>
-                <p class="p3">sxe7654</p>
-                <p class="p4">王O哲</p>
-            </div>
-            <div class="item">
-                <p class="p1">7</p>
-                <p class="p2">1500000</p>
-                <p class="p3">sxe7654</p>
-                <p class="p4">王O哲</p>
-            </div>
-            <div class="item">
-                <p class="p1">8</p>
-                <p class="p2">1500000</p>
-                <p class="p3">sxe7654</p>
-                <p class="p4">王O哲</p>
-            </div>
-            <div class="item">
-                <p class="p1">9</p>
-                <p class="p2">1500000</p>
-                <p class="p3">sxe7654</p>
-                <p class="p4">王O哲</p>
-            </div>
-            <div class="item">
-                <p class="p1">10</p>
-                <p class="p2">1500000</p>
-                <p class="p3">sxe7654</p>
-                <p class="p4">王O哲</p>
+            <div class="rankTable">
+                <div class="title">
+                    <p>排名</p>
+                    <p>累積獎金</p>
+                    <p>會員帳號</p>
+                    <p>名字</p>
+                </div>
+                <div class="item">
+                    <h3>4</h3>
+                    <div class="infor">
+                        <p>955455</p>
+                        <p>asdd15886</p>
+                        <p>陳聰明</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <h3>5</h3>
+                    <div class="infor">
+                        <p>884535</p>
+                        <p>tt8897</p>
+                        <p>黃順安</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <h3>6</h3>
+                    <div class="infor">
+                        <p>822354</p>
+                        <p>r002568778</p>
+                        <p>簡里安</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <h3>7</h3>
+                    <div class="infor">
+                        <p>801220</p>
+                        <p>yyyyyp333</p>
+                        <p>陳恩</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <h3>8</h3>
+                    <div class="infor">
+                        <p>766887</p>
+                        <p>qaz2582582</p>
+                        <p>彭玉暄</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <h3>9</h3>
+                    <div class="infor">
+                        <p>699874</p>
+                        <p>mm3939889</p>
+                        <p>吳承恩</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <h3>10</h3>
+                    <div class="infor">
+                        <p>612220</p>
+                        <p>h098889555</p>
+                        <p>皮世名</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
